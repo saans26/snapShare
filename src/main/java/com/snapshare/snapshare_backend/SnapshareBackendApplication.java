eea2648a46ef174974858bf9cbf054acd39651f3
saans26 <s.jalte0987@gmail.com>
2025-03-09 18:07:43 -0700

    1st commit

---
+++ src/main/java/com/snapshare/snapshare_backend/SnapshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snapshare.snapshare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapshareBackendApplication.class, args);
+	}
+
+}
